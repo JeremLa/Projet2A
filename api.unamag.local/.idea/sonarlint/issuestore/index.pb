2ae089516f0c839195285ca1375d72516ea2bd17
JeremLa <jeremy.lahore@gmail.com>
2017-06-13 17:47:35 +0200

    connection bddd + create authent bundle

---
+++ api.unamag.local/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,6 @@
+
+:
+
+.gitignore,a\5\a5cc2925ca8258af241be7e5b0381edf30266302
+=
+composer.json,3\9\3957b91a0d8450cea0115cfd5af69eac90d75897
---
+++ api.unamag.local/src/AuthenticationBundle/Entity/User.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace AuthenticationBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * User
+ *
+ * @ORM\Table(name="entity_user")
+ * @ORM\Entity(repositoryClass="AuthenticationBundle\Repository\Entity\UserRepository")
+ */
+class User
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
+
